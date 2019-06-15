34bf8ccf3c909df99dae7dd48d8ed4dec09d0b4a
milkslapper <oscar.valenzuela69@gmail.com>
2019-06-14 20:31:06 -0700

    added styling nav bar

--- village/src/components/NavBar.js
+++ village/src/components/NavBar.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
+// import styled from 'styled-components'
 
 export class NavBar extends Component {
     render() {
         return (
             <div>
-                <div>
-                    <NavLink to='/'>Home</NavLink>
-                    <NavLink to='/smurf-form'>Add Smurf</NavLink>
+                <div className="nav-bar">
+                    <NavLink className="nav" to='/'>Home</NavLink>
+                    <NavLink  className="nav" to='/smurf-form'>Add Smurf</NavLink>
                 </div>
             </div>
         )
     }
 }
+
